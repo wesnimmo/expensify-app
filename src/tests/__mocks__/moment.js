8e13febb1b26d53aa9059c01ed6b8e8088f97027
Wesley Nimmo <wesnimmo@gmail.com>
2018-11-30 10:11:29 -0800

    Added a tests

---
+++ src/tests/__mocks__/moment.js
@@ -0,0 +1,5 @@
+const moment = require.requireActual('moment');
+
+export default () => {
+	return moment(timestamp);
+};
--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,45 @@
 //entry -> output
 
-const path = require('path');
+module.exports = (env) => {
 
-module.exports = {
-	entry: './src/app.js',
-	output: {
-		path: path.join(__dirname, 'public'),
-		filename: 'bundle.js'
-	},
-	module: {
-		rules: [
-			{
-				loader: 'babel-loader',
-				test: /\.js$/,
-				exclude: /node_modules/
-			}, 
+	const isProduction = env === 'production';
 
-			{
-				test: /\.s?css$/,
-				use: [
-					'style-loader',
-					'css-loader',
-					'sass-loader'
-				]
-			}
-		]
-	},
-	devtool: 'cheap-module-eval-source-map',
-	devServer: {
-		contentBase: path.join(__dirname, 'public'),
-		historyApiFallback: true
+	console.log('env', env);
+	return {
+		entry: './src/app.js',
+		output: {
+			path: path.join(__dirname, 'public'),
+			filename: 'bundle.js'
+		},
+		module: {
+			rules: [
+				{
+					loader: 'babel-loader',
+					test: /\.js$/,
+					exclude: /node_modules/
+				}, 
+
+				{
+					test: /\.s?css$/,
+					use: [
+						'style-loader',
+						'css-loader',
+						'sass-loader'
+					]
+				}
+			]
+		},
+		devtool: isProduction ? 'source-map' : 'cheap-module-eval-source-map',
+		devServer: {
+			contentBase: path.join(__dirname, 'public'),
+			historyApiFallback: true
+		}
 	}
 };
+
+const path = require('path');
+
+
 
 
 
